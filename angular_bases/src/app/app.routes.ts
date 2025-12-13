93b4b7f0c17b8a94828eba86af6934e024adf87c
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-13 19:48:25 -0400

    Cambios 4

--- angular_bases/src/app/app.routes.ts
+++ angular_bases/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 //import {CounterPageComponents} from './pages/counter/counter-page.components';
 import {HeroPageComponent} from './pages/hero/hero-page.component';
+import {CounterPageComponents} from './pages/counter/counter-page.components';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HeroPageComponent
+    component: CounterPageComponents,
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent,
   }
 ];
--- angular_bases/src/app/pages/hero/hero-page.component.ts
+++ angular_bases/src/app/pages/hero/hero-page.component.ts
@@ -1,8 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {describe} from 'vitest';
 
 @Component({
-  templateUrl: './pages/hero-page/hero-page.component.html'
+  templateUrl: './hero-page.component.html',
+
 })
 export class HeroPageComponent{
+  name=signal('Iron man');
+  age=signal(45);
+  heroDescription=computed(()=>{
+    const description=  ` ${this.name()} ${this.age()} ` ;
+    return description;
+  })
+  capitalizedName=computed(()=>this.name().toUpperCase());
+ changeHero(){
+    this.name.set('Spiderman');
+ }
+ changeAge(){
+    this.age.set(22);
+ }
+  resetForm(){
+    this.name.set('iron man');
+    this.age.set(45)
+  }
 
 }
